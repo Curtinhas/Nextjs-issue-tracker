5b346b2e14e5e570279d7ea3cb8ab696436d6d37
Curtinhas <carlos_curtinhas@sapo.pt>
2024-04-26 18:26:46 +0100

    implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,23 +2,30 @@
 
 import "easymde/dist/easymde.min.css";
 
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text } from "@radix-ui/themes";
 import { Controller, useForm } from "react-hook-form";
 
 import SimpleMDE from "react-simplemde-editor";
 import axios from "axios";
+import { createIssueSchema } from "@/app/validationSchemas";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const [error, setError] = useState("");
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   return (
     <div className="max-w-xl ">
       {error && (
@@ -43,6 +50,11 @@ const NewIssuePage = () => {
           className="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-300 dark:border-gray-300 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           {...register("title")}
         />
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -50,6 +62,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
 
         <Button>Submit New Issue</Button>
       </form>
